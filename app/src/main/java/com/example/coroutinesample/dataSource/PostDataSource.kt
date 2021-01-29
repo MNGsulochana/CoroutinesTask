afaee97670c6a459c2a93b255f0912b82f8099f7
Sulochana G <naga.sulochana@mobiotics.com>
2021-01-29 10:38:06 +0530

    removed unused code

--- app/src/main/java/com/example/coroutinesample/dataSource/PostDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.coroutinesample.dataSource
-
-import com.example.coroutinesample.datamodel.PostModelItem
-
-
-interface PostDataSource {
-    suspend fun insertPost(modelList:List<PostModelItem>)
-    suspend fun getTasks(): Result<List<PostModelItem>>
-}
--- app/src/main/java/com/example/coroutinesample/di/module/ApiModule.kt
+++ app/src/main/java/com/example/coroutinesample/di/module/ApiModule.kt
@@ -1,11 +1,6 @@
 package com.example.coroutinesample.di.module
 
-import com.example.coroutinesample.adapter.PostAdapter
-import com.example.coroutinesample.api.ApiInterface
-import com.example.coroutinesample.dataSource.PostDataSource
-import com.example.coroutinesample.dataSource.PostsLocalDataSource
-import com.example.coroutinesample.db.AppDataBase
-import com.example.coroutinesample.di.scope.ActivityScope
+
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
